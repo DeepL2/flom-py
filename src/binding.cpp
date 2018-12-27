f65bc9f259a1ed5441829356fb0e9fc31d0de730
coord.e <me@coord-e.com>
2018-12-27 12:09:26 +0000

    Add: Add some methods to handle frames

--- src/binding.cpp
+++ src/binding.cpp
@@ -27,5 +27,22 @@ PYBIND11_MODULE(flom, m) {
     .def("dump_json", [](flom::Motion const& motion, std::string const& filename) {
         std::ofstream f(filename);
         motion.dump_json(f);
-      });
+      })
+    .def("frame_at", &flom::Motion::frame_at);
+
+  py::class_<flom::Frame>(m, "Frame")
+    .def_readwrite("positions", &flom::Frame::positions)
+    .def_readwrite("effectors", &flom::Frame::effectors);
+
+  py::class_<flom::Effector>(m, "Effector")
+    .def_readwrite("location", &flom::Effector::location)
+    .def_readwrite("rotation", &flom::Effector::rotation);
+
+  py::class_<flom::Location>(m, "Location")
+    .def_readwrite("weight", &flom::Location::weight)
+    .def_readwrite("vec", &flom::Location::vec);
+
+  py::class_<flom::Rotation>(m, "Rotation")
+    .def_readwrite("weight", &flom::Rotation::weight)
+    .def_readwrite("quat", &flom::Rotation::quat);
 }
