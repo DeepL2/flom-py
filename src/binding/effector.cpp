d0d1a7f028d4335dfae893f451ecefba2aa7dacb
coord.e <me@coord-e.com>
2018-12-30 09:59:07 +0000

    Fix: Include stl.h to enable stl container conversion of pybind11

--- src/binding/effector.cpp
+++ src/binding/effector.cpp
@@ -3,6 +3,7 @@
 #include <pybind11/numpy.h>
 #include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include <boost/qvm/quat_access.hpp>
 #include <boost/qvm/vec_access.hpp>
--- src/binding/frame.cpp
+++ src/binding/frame.cpp
@@ -2,6 +2,7 @@
 
 #include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include "declarations.hpp"
 
