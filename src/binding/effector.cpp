d58dfe7fa1051c41903315efae9cf0ca656684e4
coord.e <me@coord-e.com>
2019-02-04 06:05:28 +0000

    Add: Add convenient methods to Location and ROtation

--- src/binding/effector.cpp
+++ src/binding/effector.cpp
@@ -80,10 +80,17 @@ void define_effector(py::module &m) {
 
   py::class_<flom::Location>(m, "Location")
       .def(py::init<>())
+      .def(py::init<double, double, double>(), py::arg("x"), py::arg("y"),
+           py::arg("z"))
       .def(py::init<const flom::Location::value_type &>())
       .def_property("vector", &flom::Location::vector,
                     &flom::Location::set_vector,
                     py::return_value_policy::reference_internal)
+      .def_property("x", &flom::Location::x, &flom::Location::set_x)
+      .def_property("y", &flom::Location::y, &flom::Location::set_y)
+      .def_property("z", &flom::Location::z, &flom::Location::set_z)
+      .def("xyz", &flom::Location::xyz)
+      .def("set_xyz", &flom::Location::set_xyz)
       .def(py::self == py::self)
       .def(py::self != py::self)
       .def(py::self + py::self)
@@ -95,6 +102,8 @@ void define_effector(py::module &m) {
 
   py::class_<flom::Rotation>(m, "Rotation")
       .def(py::init<>())
+      .def(py::init<double, double, double, double>(), py::arg("w"),
+           py::arg("x"), py::arg("y"), py::arg("z"))
       .def(py::init<const flom::Rotation::value_type &>())
       .def_property(
           "quaternion",
@@ -103,6 +112,12 @@ void define_effector(py::module &m) {
             rot.set_quaternion(Eigen::Quaternion<double>(coeffs));
           },
           py::return_value_policy::reference_internal)
+      .def_property_readonly("w", &flom::Rotation::w)
+      .def_property_readonly("x", &flom::Rotation::x)
+      .def_property_readonly("y", &flom::Rotation::y)
+      .def_property_readonly("z", &flom::Rotation::z)
+      .def("wxyz", &flom::Rotation::wxyz)
+      .def("set_wxyz", &flom::Rotation::set_wxyz)
       .def(py::self == py::self)
       .def(py::self != py::self)
       .def(py::self + py::self)
